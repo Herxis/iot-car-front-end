b05a4b2a3fa497ccc79b4847c582f5931cb58a6a
Herxis <eber_torres3600@Outlook.com>
2024-05-16 12:09:54 -0600

    Update main.js

--- frontend/control/web/js/main.js
+++ frontend/control/web/js/main.js
@@ -7,7 +7,7 @@ let mensaje = document.getElementById('mensaje');
 
 function calllAPI(valor){
     // Hacer una petición para un usuario con ID especifico
-    axios.get('http://localhost/iot-car-control/backend/apis/setRegistro.php?valorEstatus='+valor)
+    axios.get('18.171.249.102/iot-car-control/backend/apis/setRegistro.php?valorEstatus='+valor)
       .then(function (response) {
         // manejar respuesta exitosa
         console.log(response);
